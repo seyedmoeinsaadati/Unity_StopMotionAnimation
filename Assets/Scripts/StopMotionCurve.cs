f2eec02a66b0df93efc3fceb6350ca7e333b923d
Seyed Moein Saadati <saadati.moin@gmail.com>
2023-05-18 11:33:44 +0330

    Add StopMotionCurve.cs

---
+++ Assets/Scripts/StopMotionCurve.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Moein.Animation
+{
+    [ExecuteInEditMode]
+    public class StopMotionCurve : MonoBehaviour
+    {
+        public bool autoUpdate = false;
+        [SerializeField] private FrameRate frameRate;
+        [SerializeField] AnimationUtility.TangentMode tangentMode;
+        [SerializeField] WeightedMode weightedMode;
+
+        [SerializeField] private ParticleSystem.MinMaxCurve curve;
+
+        [SerializeField] private float previewOffset = -1;
+
+        private AnimationCurve inCurve => curve.curveMin;
+        private AnimationCurve outCurve => curve.curveMax;
+
+        private void Update()
+        {
+            if (autoUpdate) RefreshOutCurve();
+        }
+
+        private void RefreshOutCurve()
+        {
+            var curve = AnimationClipUtility.ReCurve(inCurve, tangentMode, tangentMode, weightedMode, (int) frameRate,
+                previewOffset);
+            outCurve.keys = curve.keys;
+            outCurve.postWrapMode = curve.postWrapMode;
+            outCurve.preWrapMode = curve.preWrapMode;
+        }
+    }
+}
